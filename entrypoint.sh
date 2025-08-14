a8b10ad1c314ae5a8c175e3b447cf1c627f2cc12
Erik Donath <erikdonath2006@gmail.com>
2025-08-14 20:19:27 +0200

    Improved entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Start the Cron daemon in the background
+trap 'service cron stop; su -s /bin/bash znuny -c "/opt/znuny/bin/znuny.Daemon.pl stop"; exit 0' SIGTERM
+
 service cron start
-
-# Start the Znuny Daemon as the Znuny user (important for background jobs)
 su -s /bin/bash znuny -c "/opt/znuny/bin/znuny.Daemon.pl start"
-
-# Start Apache in the foreground
 exec apache2ctl -D FOREGROUND
